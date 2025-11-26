dd6cafd9523a34c160f231e615f89992cce448f6
SiccoMode <133100186+SiccoMode@users.noreply.github.com>
2025-11-26 11:35:16 +0100

    Added initial framework for class models, with generic placeholder attributes

--- informationModel/bim/models.py
+++ informationModel/bim/models.py
@@ -4,3 +4,22 @@ from django.db import models
 class Floor(models.Model):
     storey = models.IntegerField()
     
+class Zone(models.Model):
+    length = models.DecimalField()
+    width = models.DecimalField()
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+
+class BuildingElement(models.Model):
+    height = models.IntegerField()
+    width = models.IntegerField()
+    length = models.IntegerField()
+    zone = models.ManyToManyField()
+
+    class Meta:
+        abstract = True
+
+class Column(models.Model):
+    geometry = models.CharField()
+
+class Wall(models.Model):
+    color = models.CharField()
