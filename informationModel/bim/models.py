a1996339ca3a8c8248535d5a87ddee8def5a3e2b
SiccoMode <133100186+SiccoMode@users.noreply.github.com>
2025-11-26 14:06:53 +0100

    modeled manytomany relationships and added uuid to classes

--- informationModel/bim/models.py
+++ informationModel/bim/models.py
@@ -1,21 +1,31 @@
+import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Floor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
     storey = models.IntegerField()
     
 class Zone(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
     length = models.DecimalField()
     width = models.DecimalField()
+    x = models.DecimalField()
+    y = models.DecimalField()
     floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+    building_elements = models.ManyToManyField("BuildingElement", on_delete=models.CASCADE)
 
 class BuildingElement(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable = False)
+    name = models.CharField(max_length=100)
+    description = models.CharField()
     height = models.IntegerField()
     width = models.IntegerField()
     length = models.IntegerField()
-    zone = models.ManyToManyField()
-    
+    zones = models.ManyToManyField(Zone)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
 
     class ProgressState(models.TextChoices):
         READYFORCONSTRUCTION = "RE", _("Ready for Construction")
@@ -28,7 +38,7 @@ class BuildingElement(models.Model):
     class Meta:
         abstract = True
 
-class Column(models.Model):
+class Column(BuildingElement):
     class GeometryType:
         CIRCULAR = "C"
         SQUARE = "SQ"
@@ -36,5 +46,5 @@ class Column(models.Model):
     
     geometry = models.CharField(choices=GeometryType)
 
-class Wall(models.Model):
+class Wall(BuildingElement):
     color = models.CharField()
