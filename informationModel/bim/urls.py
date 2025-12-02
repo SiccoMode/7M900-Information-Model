1fb9541ba3d61580f1f646ec538010350d945a22
SiccoMode <133100186+SiccoMode@users.noreply.github.com>
2025-12-02 14:47:45 +0100

    updates views to include Floors, Zones and BuildingElement classes

--- informationModel/bim/urls.py
+++ informationModel/bim/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"),   
+    path('floors/', views.getFloors, name='floors'),
+    path('zones/', views.getZones, name='zones'),
+    path('buildingelements/', views.getBuildingElements, name='buildingelements'), 
 ]
--- informationModel/bim/views.py
+++ informationModel/bim/views.py
@@ -2,6 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Floor, Zone, BuildingElement, Wall, Column
+# from .forms import Course, Student
 
 def index(request):
     return HttpResponse("Hello, world. You're at the students index.")
+
+def getFloors(request):
+    floors = Floor.objects.all()
+    context={
+        'floors': floors,
+    }
+    return render(request, "floors.html", context)
+
+def getZones(request):
+    zones = Zone.objects.all()
+    context={
+        'zones': zones,
+    }
+    return render(request, "zones.html", context)
+
+
+def getBuildingElements(request):
+    buildingElements = BuildingElement.objects.all()
+    context={
+        'buildingElements': buildingElements,
+    }
+    return render(request, "buildingelements.html", context)
+
